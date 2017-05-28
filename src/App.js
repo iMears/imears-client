9f61fb5dff10913288a921eea607acccc1731eff
imears <maxwellmears@gmail.com>
2017-05-28 15:26:16 -0700

    Get data user data from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import axios from 'axios';
+import logo  from './logo.svg';
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    const options = {
+      baseURL: process.env['REACT_APP_API_URL'],
+      headers: {'Authorization': process.env['REACT_APP_CLIENT_ID'] },
+    };
+
+    axios.get('/users', options)
+      .then((res) => this.setState({ users: res.data }))
+      .catch((err) => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +35,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="users">
+          { this.state.users.map((user) => <div key={user.id}>email: {user.email}</div>) }
+        </div>
       </div>
     );
   }
